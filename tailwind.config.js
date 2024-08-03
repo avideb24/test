d36fdcd5fddfd747df1ed2d78d7d4b0653eb1cac
Avi Debnath <debavi24@gmail.com>
2024-08-03 10:26:15 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'primary-color': '#009432',
+        'secondary-color': '#FFC312',
+        'primary-bg': '#fff',
+        'secondary-bg': '#2f3542',
+        'primary-text': '#2f3542',
+        'secondary-text': '#fff',
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+    },
+  },
+  plugins: [daisyui],
+};
